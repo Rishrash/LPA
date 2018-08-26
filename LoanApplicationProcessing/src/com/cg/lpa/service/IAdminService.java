907207558bcb373f92c3b650c263c9af473fa76a
Rishrash <Rishrash@192.168.0.11>
2018-08-26 23:40:23 +0530

    Q

--- LoanApplicationProcessing/src/com/cg/lpa/service/IAdminService.java
+++ LoanApplicationProcessing/src/com/cg/lpa/service/IAdminService.java
@@ -1,5 +1,9 @@
 package com.cg.lpa.service;
 
 public interface IAdminService {
+	/*
+	 * TODO : Update Info Of Long Program TODO : View List of loan applications
+	 * approved/accepted (waiting for interview)/rejected for a loan program.
+	 */
 
 }
